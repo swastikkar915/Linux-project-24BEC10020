b29aa8d0a129eb54088d502d42d797e1640e5cb4
swastikkar915 <swastikkartiku@gmail.com>
2026-03-31 00:58:21 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
-# Author: Shabi | Course: Open Source Software
+# Author: Swastik | Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
